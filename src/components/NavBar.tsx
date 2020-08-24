01d9458baac6116f5d7566d3d5d0037fd97b9b96
Kenneth Ng <nwc27101994@yahoo.com.hk>
2020-08-24 15:45:40 +0800

    Update style

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Link, Flex, Button } from "@chakra-ui/core";
+import { Box, Link, Flex, Button, Heading } from "@chakra-ui/core";
 import NextLink from "next/link";
 import { useMeQuery, useLogoutMutation } from "../generated/graphql";
 import { isServer } from "../utils/isServer";
@@ -31,7 +31,12 @@ export default function NavBar() {
   //   user login
   else {
     body = (
-      <Flex>
+      <Flex align="center">
+        <NextLink href="/create-post">
+          <Button as={Link} mr={4}>
+            create post
+          </Button>
+        </NextLink>
         <Box mr={2}>{data.me.username}</Box>
         <Button
           variant="link"
@@ -47,7 +52,14 @@ export default function NavBar() {
   }
   return (
     <Flex zIndex={1} position="sticky" top={0} bg="tomato" p={4}>
-      <Box ml={"auto"}>{body}</Box>
+      <Flex flex={1} m="auto" maxWidth={800} align="center">
+        <NextLink href="/">
+          <Link>
+            <Heading>LiReddit</Heading>
+          </Link>
+        </NextLink>
+        <Box ml={"auto"}>{body}</Box>
+      </Flex>
     </Flex>
   );
 }
