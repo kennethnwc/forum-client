91e73454906e7c1a9d4e6b2216b3907220ad2703
Kenneth Ng <nwc27101994@yahoo.com.hk>
2020-08-24 15:05:01 +0800

    single post query and fixed typo

---
+++ src/pages/post/[id].tsx
@@ -0,0 +1,43 @@
+import { Box, Heading } from "@chakra-ui/core";
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
+import React from "react";
+import { Layout } from "../../components/Layout";
+import { usePostQuery } from "../../generated/graphql";
+import { createUrqlClient } from "../../utils/createUrqlClient";
+
+interface Props {}
+
+const Post: React.FC<Props> = () => {
+  const router = useRouter();
+  const intId =
+    typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
+  const [{ data, error, fetching }] = usePostQuery({
+    pause: intId === -1,
+    variables: { id: intId },
+  });
+  if (fetching) {
+    return (
+      <Layout>
+        <div>Loading...</div>
+      </Layout>
+    );
+  }
+
+  if (!data?.post) {
+    return (
+      <Layout>
+        <Box>Could not find post</Box>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <Heading mb={4}>{data.post.title}</Heading>
+      {data.post.text}
+    </Layout>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
