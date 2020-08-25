2d416ab521bd937949c3ee9dde1791d86cb46c51
Kenneth Ng <nwc27101994@yahoo.com.hk>
2020-08-25 16:00:21 +0800

    apollo conversion 3.2 fixed ssr with headers

--- src/utils/withApollo.ts
+++ src/utils/withApollo.ts
@@ -1,31 +1,38 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { withApollo as createWithApollo } from "next-apollo";
+import { NextPageContext } from "next";
 import { PaginatedPosts } from "../generated/graphql";
+import { createWithApollo } from "./createWithApollo";
 
-export const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  credentials: "include",
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          posts: {
-            keyArgs: [], // if we have search query, we can put it in ["query"], that means ["cursor"] wont work
-            merge(
-              existing: PaginatedPosts | undefined,
-              incoming: PaginatedPosts
-            ): PaginatedPosts {
-              console.log(existing, incoming);
-              return {
-                ...incoming,
-                posts: [...(existing?.posts || []), ...incoming.posts],
-              };
+const createClient = (ctx: NextPageContext) =>
+  new ApolloClient({
+    uri: "http://localhost:4000/graphql",
+    credentials: "include",
+    headers: {
+      cookie:
+        (typeof window === "undefined" ? ctx.req?.headers.cookie : undefined) ||
+        "",
+    },
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            posts: {
+              keyArgs: [], // if we have search query, we can put it in ["query"], that means ["cursor"] wont work
+              merge(
+                existing: PaginatedPosts | undefined,
+                incoming: PaginatedPosts
+              ): PaginatedPosts {
+                console.log(existing, incoming);
+                return {
+                  ...incoming,
+                  posts: [...(existing?.posts || []), ...incoming.posts],
+                };
+              },
             },
           },
         },
       },
-    },
-  }),
-});
+    }),
+  });
 
-export const withApollo = createWithApollo(client);
+export const withApollo = createWithApollo(createClient);
