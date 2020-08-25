6433d0fc88d211ba3c851dabbb4ded7c3a8e81c1
Kenneth Ng <nwc27101994@yahoo.com.hk>
2020-08-25 14:43:11 +0800

    Switch to apollo and setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,23 @@
 import { ColorModeProvider, CSSReset, ThemeProvider } from "@chakra-ui/core";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import theme from "../theme";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  credentials: "include",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <ThemeProvider theme={theme}>
-      <ColorModeProvider>
-        <CSSReset />
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <ColorModeProvider>
+          <CSSReset />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
